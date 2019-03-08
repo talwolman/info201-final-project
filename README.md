4db814caec506292a1bb11cedfc91fdbb02a1b2a
tal <twolman@uw.edu>
2019-03-08 12:13:35 -0800

    updated part of read me and added structure

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # a9 - Info Final Project 
 Final project for info 201. Creating data visualizations for compiled data using R and Shiny to support interactive features. 
+
+## Group Members 
+- Tal Wolman
+- Ben Weber
+- Ivan Trindev 
+- Cooper Teixeira 
+
+## Project Description 
+needs to be updated before we turn in
--- ui.R
+++ ui.R
@@ -7,7 +7,50 @@
 library("shiny")
 
 ui <- fluidPage(
+  titlePanel("title of project"),
   
+  mainPanel(
+    tabsetPanel(type = "tabs",
+                 # CHANGE THE NAME OF YOUR TAB TO BE ABOUT YOUR TOPIC!!!!! 
+                 tabPanel("Home",
+                          h1("Title of Page"),
+                          p("a GOOD and GRAMMATICALLY CORRECT description of our project")
+                          ),
+                
+                 tabPanel("Tal",
+                          h1("Title of Page"),
+                          p("an introduction to the question you are asking"),
+                          # insert your plot call here
+                          # insert and widgets you will need 
+                          p("a GOOD and GRAMMATICALLY CORRECT description of your plot")
+                          ),
+                
+                 tabPanel("Ben", 
+                          h1("Title of Page"),
+                          p("an introduction to the question you are asking"),
+                          # insert your plot call here
+                          # insert and widgets you will need 
+                          p("a GOOD and GRAMMATICALLY CORRECT description of your plot")
+                          ),
+                
+                 tabPanel("Ivan", 
+                          h1("Title of Page"),
+                          p("an introduction to the question you are asking"),
+                          # insert your plot call here
+                          # insert and widgets you will need 
+                          p("a GOOD and GRAMMATICALLY CORRECT description of your plot")
+                          ),
+                
+                 tabPanel("Cooper", 
+                          h1("Title of Page"),
+                          p("an introduction to the question you are asking"),
+                          # insert your plot call here
+                          # insert and widgets you will need 
+                          p("a GOOD and GRAMMATICALLY CORRECT description of your plot")
+                          )
+                 
+                 )
   
-  
+    
+  )
 )
